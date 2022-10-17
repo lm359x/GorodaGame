a43faa0ce57ccbf6accd243709be9d6b351c5106
cognosce <dima.chukov.02@mail.ru>
2022-10-17 19:39:02 +0300

    Play3r t3st cr3at3d

--- src/test/java/com/cities/game/DatabaseLoaderTest.java
+++ src/test/java/com/cities/game/DatabaseLoaderTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseLoaderTest {
 
-    DatabaseLoader databaseLoader = Mockito.mock(DatabaseLoader.class);
+    private final DatabaseLoader databaseLoader = Mockito.mock(DatabaseLoader.class);
 
     @Test
     void loadDatabase() {

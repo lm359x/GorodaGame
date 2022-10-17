c182183569559c78758b2c7c909910ee2532eac0
= <=>
2022-10-17 19:34:15 +0300

    add3d meth0d to r3ad l1st 0f cities

--- src/main/java/com/cities/game/DatabaseLoader.java
+++ src/main/java/com/cities/game/DatabaseLoader.java
@@ -1,10 +1,25 @@
 package com.cities.game;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class DatabaseLoader {
 
-    public void loadDatabase(File csvFile) {
+    public Set<String> loadDatabase(File csvFile) {
 
+        Set<String> cities = new HashSet<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("city.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                cities.add(values[3]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return cities;
     }
 }
